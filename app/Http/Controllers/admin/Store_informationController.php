4c00fc7c50a9b22ec155af36e6577d473a3cccd8
xxmaikuxx <xxmaikuxx@gmail.com>
2023-07-12 22:49:17 +0900

    7/12 作業

--- app/Http/Controllers/admin/Store_informationController.php
+++ app/Http/Controllers/admin/Store_informationController.php
@@ -5,9 +5,8 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Stores;
-use App\Models\Store_vacation;
-use App\Models\Store_facilities;
-use App\Models\Store_images;
+
+
 
 class Store_informationController extends Controller
 {
@@ -18,26 +17,8 @@ class Store_informationController extends Controller
 
     public function store(Request $request){
         $stores = new Stores();
-        $store_vacation = new Store_vacation();
-        $store_facilities = new Store_facilities();
-        $store_images = new Store_images();
 
         $stores->name = $request->input('name');
-        $stores->city = $request->input('city');
-        $stores->address1 = $request->input('address1');
-        $stores->address2 = $request->input('address2');
-        $stores->price = $request->input('price');
-        $stores->start_time = $request->input('start_time');
-        $stores->end_time = $request->input('end_time');
-        $stores->rest_start_time = $request->input('rest_start_time');
-        $stores->rest_end_time = $request->input('rest_end_time');
-        $stores->menu = $request->input('menu');
-        $stores->introduction = $request->input('introduction');
-        $stores->url = $request->input('url');
-        $stores->tel = $request->input('tel');
-        $store_images->store_images = $request->input('store_images');
-        $store_vacation->store_vacation = $request->input('store_vacation');
-        $store_facilities->store_facilities = $request->input('store_facilities');
         
         $stores->save();
 
--- app/Models/Stores.php
+++ app/Models/Stores.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Stores extends Model
 {
     use HasFactory;
+
+    protected $table = 'stores';
+
+   
+
+    protected $fillable = [
+        'name'
+    ];
 }
